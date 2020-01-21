2d7c92ed94d04bc67504e7dd13266784e1c14381
soundaryakella <54535547+soundaryakella@users.noreply.github.com>
2020-01-21 22:25:59 +0530

    Update P1.java

--- src/com/test/t1/P1.java
+++ src/com/test/t1/P1.java
@@ -5,6 +5,7 @@ public class P1 {
     public static void main(String[] args) {
     System.out.println("Hello Java"); 
         System.out.println("Hello hi");
+        System.out.println("Hello bye");
     }
 
 }
