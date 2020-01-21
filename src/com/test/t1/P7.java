b5f5ca04d825a8ef2ee47176559f7edc7d4a40ae
soundarya-kella <soundarya.kella@games24x7.com>
2020-01-21 22:24:19 +0530

    hcgch

---
+++ src/com/test/t1/P7.java
@@ -0,0 +1,10 @@
+package com.test.t1;
+
+public class P7 {
+
+    public static void main(String[] args) {
+        System.out.println("should not merge with master");
+
+    }
+
+}
---
+++ src/com/test/t1/P9.java
@@ -0,0 +1,8 @@
+package com.test.t1;
+
+public class P9 {
+    private void syso() {
+        // TODO Auto-generated method stub
+
+    }
+}
